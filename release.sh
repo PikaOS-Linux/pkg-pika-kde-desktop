f9de0ffb8e568f30c6196039e2e7ffb398bfd93d
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-25 11:13:51 +0300

    add github builder

---
+++ release.sh
@@ -0,0 +1,14 @@
+# Sign the packages
+dpkg-sig --sign builder ./output/pika-kde-desktop*.deb
+
+# Pull down existing ppa repo db files etc
+rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
+
+# Remove our existing package from the repo
+reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-kde-desktop*)'
+
+# Add the new package to the repo
+reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-kde-desktop*.deb
+
+# Push the updated ppa repo to the server
+rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
